0c56357014021f7baa3e31f8d62761c8f3c92a96
Darrel2018 <darrelp2000@gmail.com>
2026-01-12 10:52:16 +0200

    Lessons 1 and 2 done

---
+++ Unit0/Lesson01/lesson01.js
@@ -0,0 +1,14 @@
+3 + 3
+3 / 0
+console.log("Hello, Node.js");
+let name = "darrel";
+console.log(name)
+
+class Goat {
+eat(foodType){
+console.log(`I love eating ${foodType}`);
+}
+}
+
+let billy = new Goat();
+billy.eat("tin cans");
