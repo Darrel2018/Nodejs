3e03009c5863890f8a124bfc09e4d08f845f791e
Darrel2018 <darrelp2000@gmail.com>
2026-02-16 12:11:40 +0200

    Chapters 1 - 4 completed

---
+++ Book2/Chapter2/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => { // called when request to /about comes in
+    res.sendFile(path.resolve(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => { //called when request to /contact comes
+    res.sendFile(path.resolve(__dirname, 'contact.html'));
+});
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000");
+});
