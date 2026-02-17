893d169b3d50d4b284ba76ccef3d4f026c99df15
Darrel2018 <darrelp2000@gmail.com>
2026-02-17 13:07:06 +0200

    completed chapters 7 - 9

--- BlogPost/blog/index.js
+++ BlogPost/blog/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = new express();
-const path = require('path');
 const mongoose = require('mongoose');
-const BlogPost = require('./models/BlogPost.js');
+const fileUpload = require('express-fileupload');
+const newPostController = require('./controllers/newPost');
+const homeController = require('./controllers/home');
+const storePostController = require('./controllers/storePost');
+const getPostController = require('./controllers/getPost');
+const validateMiddleware = require("./middleware/validateMiddleware");
 
 mongoose.connect('mongodb://localhost:27017/blogdb');
 
@@ -12,19 +16,15 @@ db.once("open", () => {
     console.log("Successfully connected to MongoDB using Mongoose!");
 });
 
-
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(fileUpload());
+app.use('/posts/store', validateMiddleware);
 
-app.get('/', async (req, res) => {
-    const blogposts = await BlogPost.find({});
-    res.render('index', {
-        blogposts
-    });
-});
+app.get('/', homeController);
 
 app.get('/about', (req, res) => {
     res.render('about');
@@ -34,27 +34,11 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
-app.get('/post/:id', async (req, res) => {
-    const blogpost = await BlogPost.findById(req.params.id);
-    res.render('post', {
-        blogpost
-    });
-});
+app.get('/post/:id', getPostController);
 
-app.get('/posts/new', (req, res) => {
-    res.render('create');
-});
+app.get('/posts/new', newPostController);
 
-app.post('/posts/store', async (req, res) => {
-    try {
-        // model creates a new doc with browser data
-        await BlogPost.create(req.body);
-        res.redirect('/');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error creating blog post');
-    }
-});
+app.post('/posts/store', storePostController);
 
 app.listen(3000, () => {
     console.log('App listening on port 3000');
